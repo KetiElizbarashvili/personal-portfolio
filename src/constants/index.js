e1f6e69aab238f4c19450176de0ab9808c851833
KetiElizbarashvili <elizbarashviliketi44@gmail.com>
2023-12-02 01:38:08 +0400

    4234

--- src/constants/index.js
+++ src/constants/index.js
@@ -223,7 +223,7 @@ const testimonials = [
 
 const projects = [
   {
-    name: "Car Rent",
+    name: "proekti 1",
     description:
       "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
     tags: [
@@ -240,11 +240,11 @@ const projects = [
         color: "pink-text-gradient",
       },
     ],
-    image: carrent,
+   
     source_code_link: "https://github.com/",
   },
   {
-    name: "Job IT",
+    name: "2",
     description:
       "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
     tags: [
@@ -261,11 +261,11 @@ const projects = [
         color: "pink-text-gradient",
       },
     ],
-    image: jobit,
+    
     source_code_link: "https://github.com/",
   },
   {
-    name: "Trip Guide",
+    name: "3",
     description:
       "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
     tags: [
@@ -282,7 +282,7 @@ const projects = [
         color: "pink-text-gradient",
       },
     ],
-    image: tripguide,
+    
     source_code_link: "https://github.com/",
   },
 ];
