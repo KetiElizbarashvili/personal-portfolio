54ea1b7765c939406517d82d4d43d06db50f0723
KetiElizbarashvili <elizbarashviliketi44@gmail.com>
2023-12-05 20:25:53 +0400

    tech stack added

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -7,7 +7,8 @@ const Tech = () => {
     <>
       <div className="flex flex-row flex-wrap justify-center gap-10">
         {technologies.map((technology) => (
-          <div className="w-2 h-2" key={technology.name}>
+          <div className="w-28 h-28" key={technology.name}>
+            <BallCanvas icon={technology.icon} />
           </div>
         ))}
       </div>
